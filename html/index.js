7f4135d0d3b3e27c50fc574d960f6f73551769e6
tds455 <92926829+tds455@users.noreply.github.com>
2024-02-01 11:25:45 +0900

    Update index.html and index.js with reorganised functions

--- html/index.js
+++ html/index.js
@@ -1,21 +1,39 @@
 
 // Process form input and create API url
 
-// const apiUrl = 'https://aviation-edge.com/v2/public/flightsHistory?key=$AEKEY&code=JFK&type=arrival&date_from=2023-12-01&flight_number=4764';
+const apiUrl = 'https://asia-northeast2-delaychecker-412510.cloudfunctions.net/FlightAwareAPIv2?code=KIX&type=arrival&date_from=2024-01-15&date_to=2024-01-16&flight_number=711';
 
 // Make a GET request
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-      // Set footer to show error message
-    }
-    return response.json();
-  })
-  .then(data => {
-    // For now log data to console while testing
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+
+InputValidation()
+  // Perform Input Validation
+
+  // Error Checking
+
+  // Pass Parameters
+  let response = MakeRequest()
+
+  // if response = error
+  // Update footer to show error message
+
+  // Otherwise, CreateCards()
+
+MakeRequest()
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+        // Set footer to show error message
+      }
+      return response.json();
+    })
+    .then(data => {
+      // For now log data to console while testing
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+
+CreateCards()
